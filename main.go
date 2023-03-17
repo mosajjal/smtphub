1a72b849920bd9339d198a4a0e21827e166c253b
Ali Mosajjal <mosajjal@gmail.com>
2023-03-18 11:48:43 +1300

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/mosajjal/myapp/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
---
+++ pkg/logic.go
@@ -0,0 +1,7 @@
+package myapp
+
+import "fmt"
+
+func Run() {
+	fmt.Println("Hello World")
+}
